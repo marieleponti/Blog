e7dbd997e2c6e5b8c2ec544837b2d5203d64a34f
Mariele Ponti <mpon3004@students.vsu.edu>
2021-11-03 07:53:18 -0500

    added some funcionality for user registration

--- django_project/settings.py
+++ django_project/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'users.apps.Blog.Config',
     'blog.apps.BlogConfig',
     'django.contrib.admin',
     'django.contrib.auth',
